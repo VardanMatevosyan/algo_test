ca68c6ab3502a203246840749c4e37179f94fbec
VardanMatevosyan <vardanmt@gmail.com>
2025-11-15 13:33:59 +0400

    added duplicate problems solution

--- src/main/java/problems/array/Duplicates.java
+++ src/main/java/problems/array/Duplicates.java
@@ -1,7 +1,10 @@
 package problems.array;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 /**
  * Find duplicates in an array or collection.
@@ -10,15 +13,18 @@ import java.util.function.Function;
 public class Duplicates {
 
     public boolean hasDuplicate(int[] numbers) {
-        throw new RuntimeException();
+        Set<Integer> set = new HashSet<>();
+        return IntStream.of(numbers).anyMatch(n -> !set.add(n));
     }
 
     public <T> boolean hasDuplicate(List<T> array) {
-        throw new RuntimeException();
+        Set<T> set = new HashSet<>();
+        return array.stream().anyMatch(n -> !set.add(n));
     }
 
     public <T, K> boolean hasDuplicate(List<T> array, Function<T, K> keyExtractor) {
-        throw new RuntimeException();
+        Set<K> set = new HashSet<>();
+        return array.stream().anyMatch(n -> !set.add(keyExtractor.apply(n)));
     }
 
     /**
